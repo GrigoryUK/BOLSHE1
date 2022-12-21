a07c73942d360076a1d01ab9b788e4323672a7e9
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2022-12-21 19:17:40 +0500

    fix 1.0

--- src/js/components/anchor/anchor.js
+++ src/js/components/anchor/anchor.js
@@ -1,12 +1,13 @@
-import SmoothScroll from 'smooth-scroll';
-
-
+import SmoothScroll from "smooth-scroll";
 
 function anchor() {
-const scroll = new SmoothScroll('a[href*="#"]', {
+  const scroll = new SmoothScroll('a[href*="#"]', {
     updateURL: false,
- 
-});
+    speed: 300,
+    speedAsDuration: true,
+    easing: 'easeOutQuad',
+    durationMax: 1,
+  });
 }
 
 export default anchor;
